2449a799c8bfa5612a27aab650829edca98d77f6
Renan Marques <henrenan@gmail.com>
2021-08-31 22:45:02 -0300

    FINALIZDO

--- script.js
+++ script.js
@@ -56,13 +56,20 @@ const salveClient = () => {
             celular: document.getElementById('celular').value,
             cidade: document.getElementById('cidade').value,
         }
+        const index = document.getElementById('nome').dataset.index
+        if (index == 'new') {
         createClient(client)
         updateTable() 
         closeModal()
+        } else {
+            updadeClient(index, client)
+            updateTable()
+            closeModal() 
+        }
     }
 }
 
-const createRow = (client) => {
+const createRow = (client, index) => {
     const newRow = document.createElement('tr')
     newRow.innerHTML = `
         <td>${client.nome}</td>
@@ -70,8 +77,8 @@ const createRow = (client) => {
         <td>${client.celular}</td>
         <td>${client.cidade}</td>
          <td>
-            <button type="button" class="button green">editar</button>
-            <button type="button" class="button red">excluir</button>
+            <button type="button" class="button green" id="edit-${index}">Editar</button>
+            <button type="button" class="button red" id="delete-${index}">Excluir</button>
         </td>
     `
     document.querySelector('#tableClient>tbody').appendChild(newRow)
@@ -89,9 +96,36 @@ const updateTable = () => {
 
 }
 
+const fillFields = (client) => {
+    document.getElementById('nome').value = client.nome
+    document.getElementById('email').value = client.email
+    document.getElementById('celular').value = client.celular
+    document.getElementById('cidade').value = client.cidade
+    document.getElementById('nome').dataset.index = client.index 
+}
+ 
+const editClient = (index) => {
+    const client =readClient()[index]
+    client.index = index
+    fillFields(client)
+    openModal()
+}
+
 const editDelete = (event) => {
     if (event.target.type == 'button'){
-    console.log(event.target.type)
+        
+        const [action, index]= event.target.id.split('-')
+
+        if (action == 'edit') {
+            editClient(index)
+        } else {
+            const client = readClient()[index]
+            const response = confirm (`Deseja realmente excluir o cliente ${client.nome}`)
+            if (response) {
+            deleteClient(index)
+            updateTable()
+            }
+        }
     }
 }
 
